94665d707f346733b1efdf29c18866f36a804048
Mario Orlandi <morlandi@brainstorm.it>
2018-02-25 09:30:39 +0100

    Management commands to dump and delete all tasks

---
+++ django_task/management/commands/delete_all_tasks.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django.apps import apps
+from django_task.models import Task
+from django_task.app_settings import LOG_ROOT
+
+
+class Command(BaseCommand):
+
+    help = ("Delete all tasks from db tables")
+
+    def handle(self, *app_labels, **options):
+
+        # Delete all subtasks, then Tak itself
+        models = apps.get_models()
+        task_models = [model for model in models if issubclass(model, Task) and model != Task]
+        task_models.append(Task)
+        for model in task_models:
+            print('Deleting %s objects (%d) ...' % (model.__name__, model.objects.count()))
+            model.objects.all().delete()
+        print('Done.')
+        print('Dont\' forget to remove log files from folder "%s"' % LOG_ROOT)
---
+++ django_task/management/commands/dump_all_tasks.py
@@ -0,0 +1,30 @@
+from django.core.management.commands.dumpdata import Command as DumpdataCommand
+from django.apps import apps
+from django_task.models import Task
+
+
+class Command(DumpdataCommand):
+
+    def add_arguments(self, parser):
+        super(Command, self).add_arguments(parser)
+
+        # Remove 'args' action
+        args_action = [action for action in parser._actions if action.dest=='args'][0]
+        parser._remove_action(args_action)
+
+    help = ("Dumps a fixture of all tasks")
+
+    def handle(self, *app_labels, **options):
+
+        # List Task model first, then all Task subclasses
+        app_labels = [
+            'django_task.Task',
+        ]
+        models = apps.get_models()
+        task_models = [model for model in models if issubclass(model, Task) and model != Task]
+        for model in task_models:
+            app_labels.append(
+                '%s.%s' % (model._meta.app_label, model.__name__)
+            )
+
+        super(Command, self).handle(*app_labels, **options)
