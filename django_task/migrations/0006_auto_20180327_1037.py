471590128ed8cf41359da944bd786290088444ff
Mario Orlandi <morlandi@brainstorm.it>
2018-05-08 18:00:38 +0200

    Wrong migration dependency removed

--- django_task/migrations/0006_auto_20180327_1037.py
+++ django_task/migrations/0006_auto_20180327_1037.py
@@ -6,7 +6,6 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tasks', '0003_auto_20180327_1037'),
         ('django_task', '0005_auto_20180316_1838'),
     ]
 
