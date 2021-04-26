9cc6c11866a0a2568cc8d5ed010667525f6f4dca
Mario Orlandi <morlandi@brainstorm.it>
2021-04-26 17:35:26 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -2,12 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 import re
-import sys
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import find_packages, setup
 
 
 def get_version(*file_paths):
@@ -22,43 +17,24 @@ def get_version(*file_paths):
 
 
 version = get_version("django_task", "__init__.py")
-
-
-if sys.argv[-1] == 'publish':
-    try:
-        import wheel
-        print("Wheel version: ", wheel.__version__)
-    except ImportError:
-        print('Wheel library missing. Please run "pip install wheel"')
-        sys.exit()
-    os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
-    sys.exit()
-
-if sys.argv[-1] == 'tag':
-    print("Tagging the version on git:")
-    os.system("git tag -a %s -m 'version %s'" % (version, version))
-    os.system("git push --tags")
-    sys.exit()
-
 readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+history = open('CHANGELOG.rst').read().replace('.. :changelog:', '')
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-task',
     version=version,
-    description="""A Django app to run new background tasks from either admin or cron, and inspect task history from admin; based on django-rq""",
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='A Django app to run new background tasks from either admin or cron, and inspect task history from admin; based on django-rq',
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/x-rst',
     author='Mario Orlandi',
     author_email='morlandi@brainstorm.it',
     url='https://github.com/morlandi/django-task',
-    packages=[
-        'django_task',
-    ],
-    include_package_data=True,
-    #install_requires=['django>=1.11', 'django-rq>=2.1.0', 'rq>=1.1.0'],
-    license="MIT",
     zip_safe=False,
     keywords='django-task',
     classifiers=[
@@ -71,6 +47,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
